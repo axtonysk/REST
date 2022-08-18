951b24bb20d87206389122cd9862e024f7f4e519
axtonysk <axton_yew@hotmail.com>
2022-08-18 18:14:28 +0800

    Added mysql

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,27 @@
-const app = require('express')();
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
 const PORT = 8080;
 
-app.listen(PORT, () => console.log("test"));
+app.listen(PORT, ()=> console.log(`Server: http://localhost:${PORT}`))
 
-app.get('/account', (res, req) => {
- 
+app.use(express.json())
+
+app.get('/accounts', (req, res) => {
+
+    // sample; change to database
+    res.status(200).send({username: 'axtonyew', name: 'axton', gender: 'male'}) 
 });
+
+app.post('/updateprofile', (req, res) => {
+
+});
+
+
+function generateAPIKey() {
+}
+
+function hashAPIKey() {
+
+}
